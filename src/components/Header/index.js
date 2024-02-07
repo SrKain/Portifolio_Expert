c356688adeb526100b79a9c36d1d5fbe18f6d5c7
SrKain <134458062+SrKain@users.noreply.github.com>
2024-02-06 23:49:39 -0300

    ok

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,8 +3,8 @@ const Header = (props) => {
     return(
         <section className="flex h-[15vh] max-w-full max-h-full items-center px-8 my-5 justify-between gap-[10%]">
 
-                <a className='h-full' href='#'> 
-                    <img className="h-full" src="./assets/logoKauanIasin.png" />
+                <a className='h-full' href='https://portifolio-expert.vercel.app/'> 
+                    <img className="h-full" alt="logo do desenvvolvedor Front-end Kauan Iasin." src="./assets/logoKauanIasin.png" />
                 </a>
 
                 <nav className="flex w-full justify-evenly">
